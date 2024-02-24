6c821c34df73020dcdfd89a7700e758709e9e871
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-02-24 15:06:28 +0530

    aastring_reverseword_in_sentance1

---
+++ aastring_reverseword_in_sentance1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+void  reversesentence( char str[],int start,int end)
+{
+    char temp;
+    while(start<end)
+    {
+        temp=str[start];
+       str[start]=str[end];
+       str[end]=temp;
+       end--;
+       start++;
+    }
+}
+void reverseword(char str[]) {
+    int start = 0;
+    int end;
+    int length = strlen(str);
+    reversesentence(str, 0, length - 1);
+    for (end = 0; end < length; end++) {
+        if (str[end] == '.') {
+            reversesentence(str, start, end - 1);
+            start = end + 1;
+        }
+    }
+    reversesentence(str, start, length - 1);
+}
+int main()
+{ 
+    char str[20];
+
+    printf("Enter the string:-");
+    scanf("%s",&str);
+   reverseword(str);
+    printf("reverse the world:-%s",str);
+    return 0;
+}
