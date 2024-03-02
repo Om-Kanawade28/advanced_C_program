1435872f9c52891dc32803c282022b1e5e13b2b3
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-03-02 13:46:57 +0530

    Update simple.c

--- structure/simple.c
+++ structure/simple.c
@@ -5,9 +5,7 @@ int main()
     int sno;
     char sname[20];
     float per;
-}s={1,"om",90.28};
-printf("sno=%d\n",s.sno);
-printf("sno=%s\n",s.sname);
-printf("sno=%.2f\n",s.per);
+}s;
+
     return 0;
 }
